aa8388ca0312622157418392d691a5c97ab311aa
Darryl <darryltanggy@gmail.com>
2018-04-23 16:19:42 +0800

    Applied styling to Main and child components

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return(
         <div>
-            <h3>Examples</h3>
-            <p>Here are some examples on how you could use my app!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are some example locations you could use to try my app!</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Singapore'>Singapore</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Japan'>Tokyo, Japan</Link>
+                </li>
+            </ol>
         </div>
     );
 }
