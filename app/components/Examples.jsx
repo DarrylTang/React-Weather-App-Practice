1413c701c10c30a37015acd7374aa2d07e53c0cc
Darryl <darryltanggy@gmail.com>
2018-04-23 10:48:07 +0800

    Added some words

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return(
-        <h3>Examples component~</h3>
+        <div>
+            <h3>Examples component~</h3>
+            <p>Here are some examples on how you could use my app!</p>
+        </div>
     );
 }
 
